4451b6bfe01f58d7330b7584865d9e2eba61ce70
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 16:42:33 -0500

    :memo: Set Repo to JavaScript

---
+++ index.js
@@ -0,0 +1,5 @@
+const PYTHON_CHALLENGES = "https://github.com/UltiRequiem/hacker-rank-python";
+
+const TEN_DAYS_OF_JS = "https://github.com/UltiRequiem/10-days-of-js";
+
+export { PYTHON_CHALLENGES, TEN_DAYS_OF_JS };
